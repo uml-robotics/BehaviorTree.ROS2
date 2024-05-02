9497662c2b6b33034ff114809a6477e63475dcb9
Davide Faconti <davide.faconti@gmail.com>
2024-05-02 17:12:52 +0200

    more comments

--- behaviortree_ros2/include/behaviortree_ros2/tree_execution_server.hpp
+++ behaviortree_ros2/include/behaviortree_ros2/tree_execution_server.hpp
@@ -27,6 +27,8 @@ namespace BT
 /**
  * @brief TreeExecutionServer class hosts a ROS Action Server that is able
  * to load Behavior plugins, BehaviorTree.xml files and execute them.
+ *
+ * It can be customized by overriding its virtual functions.
  */
 class TreeExecutionServer
 {
@@ -84,18 +86,33 @@ protected:
   {}
 
   /**
-   * @brief onLoopAfterTick invoked after the tree is created and before the tree is executed.
+   * @brief onLoopAfterTick invoked at each loop, after tree.tickOnce().
+   * If it returns a valid NodeStatus, the tree will stop and return that status.
+   * Return std::nullopt to continue the execution.
+   *
+   * @param status The status of the tree after the last tick
   */
   virtual std::optional<BT::NodeStatus> onLoopAfterTick(BT::NodeStatus status)
   {
     return std::nullopt;
   }
 
-  // To be overridden by the user.
-  // Callback invoked when the tree execution is completed
+  /**
+   * @brief onTreeExecutionCompleted is a callback invoked after the tree execution is completed,
+   * i.e. if it returned SUCCESS/FAILURE or if the action was cancelled by the Action Client.
+   *
+   * @param status The status of the tree after the last tick
+   * @param was_cancelled True if the action was cancelled by the Action Client
+  */
   virtual void onTreeExecutionCompleted(BT::NodeStatus status, bool was_cancelled)
   {}
 
+  /**
+   * @brief onLoopFeedback is a callback invoked at each loop, after tree.tickOnce().
+   * If it returns a valid string, it will be sent as feedback to the Action Client.
+   *
+   * If you don't want to return any feedback, return std::nullopt.
+  */
   virtual std::optional<std::string> onLoopFeedback()
   {
     return std::nullopt;
