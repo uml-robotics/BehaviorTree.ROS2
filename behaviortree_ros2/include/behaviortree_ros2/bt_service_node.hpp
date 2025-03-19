e76c944e8bcbaf4558e926d860990eebf6cd5d01
Gregory LeMasurier <gregorylemasurier@gmail.com>
2025-03-19 12:51:37 -0400

    added spinning - need to check if needed

--- behaviortree_ros2/include/behaviortree_ros2/bt_service_node.hpp
+++ behaviortree_ros2/include/behaviortree_ros2/bt_service_node.hpp
@@ -315,7 +315,7 @@ inline NodeStatus RosServiceNode<T>::tick()
   // service_name in the port didn't change.
   // otherwise, create a new client
   //Temporary workaround for threading issues - Greg
-  //rclcpp::spin_some(node_);
+  srv_instance_->callback_executor.spin_some();
   if(!srv_instance_ || (status() == NodeStatus::IDLE && service_name_should_be_checked_))
   {
     std::string service_name;
